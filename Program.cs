41a5dc50ea1d4149b3c781cda176261647f14952
Sumit Patil <sumitpatil00563@gmail.com>
2024-10-17 11:00:48 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SingletonDesignPattern;
+
+Thread t1 = new Thread(() =>
+{ 
+    var obj = SingletonService.GetInstance(1);
+});
+
+Thread t2 = new Thread(() =>
+{
+    var obj = SingletonService.GetInstance(2);
+});
+
+t1.Start();
+t2.Start();
+
+t1.Join();
+t2.Join();
---
+++ SingletonService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    class SingletonService
+    {
+        public int Id {  get; private set; }
+        private SingletonService() { }
+        private static SingletonService _instance;
+        private static readonly object _instanceLock = new object();
+        public static  SingletonService GetInstance(int id)
+        {
+            if(_instance == null)
+            {
+                lock(_instanceLock)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new SingletonService();
+                        _instance.Id = id;
+                    }
+                }
+            }
+            return _instance;
+        }
+
+    }
+}
